20c8c6eb6a7d039dee20b561332f7581f97ca8b9
David Silwal <david.silwal@ekbana.info>
2020-05-05 14:06:41 +0545

    Add project files.

---
+++ ChartSeriesModel.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public class ChartSeriesModel
+    {
+        public ChartSeriesModel(string name, int value)
+        {
+            Name = name;
+            Value = value;
+
+        }
+        [JsonProperty("name")]
+        public string Name { get; }
+
+        [JsonProperty("y")]
+        public int Value { get; }
+    }
+}
